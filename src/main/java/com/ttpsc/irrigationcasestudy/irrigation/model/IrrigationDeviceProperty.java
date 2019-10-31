b186390a23a8f34e75353c19f607d36fb3142cab
kliangh <kunliang.hou@gmail.com>
2019-10-31 15:49:49 +0800

    ThingWorx-Case-Study. 1. implemented IrrigationDeviceProperty for receiving incoming request

--- src/main/java/com/ttpsc/irrigationcasestudy/irrigation/model/IrrigationDeviceProperty.java
+++ src/main/java/com/ttpsc/irrigationcasestudy/irrigation/model/IrrigationDeviceProperty.java
@@ -1,4 +1,65 @@
 package com.ttpsc.irrigationcasestudy.irrigation.model;
 
+import com.thingworx.types.primitives.structs.Location;
+
 public class IrrigationDeviceProperty {
+    private double pumpWaterPressure;
+
+    private double actualIrrigationPower;
+
+    private Location geoLocation;
+
+    private boolean irrigationState;
+
+    private int alarmState;
+
+    private Integer irrigationPowerLevel;
+
+    public double getPumpWaterPressure() {
+        return pumpWaterPressure;
+    }
+
+    public void setPumpWaterPressure(double pumpWaterPressure) {
+        this.pumpWaterPressure = pumpWaterPressure;
+    }
+
+    public double getActualIrrigationPower() {
+        return actualIrrigationPower;
+    }
+
+    public void setActualIrrigationPower(double actualIrrigationPower) {
+        this.actualIrrigationPower = actualIrrigationPower;
+    }
+
+    public Location getGeoLocation() {
+        return geoLocation;
+    }
+
+    public void setGeoLocation(Location geoLocation) {
+        this.geoLocation = geoLocation;
+    }
+
+    public boolean getIrrigationState() {
+        return irrigationState;
+    }
+
+    public void setIrrigationState(boolean irrigationState) {
+        this.irrigationState = irrigationState;
+    }
+
+    public int getAlarmState() {
+        return alarmState;
+    }
+
+    public void setAlarmState(int alarmState) {
+        this.alarmState = alarmState;
+    }
+
+    public Integer getIrrigationPowerLevel() {
+        return irrigationPowerLevel;
+    }
+
+    public void setIrrigationPowerLevel(Integer irrigationPowerLevel) {
+        this.irrigationPowerLevel = irrigationPowerLevel;
+    }
 }
